34063afef121bea907aa10c7e4440fcf70493aa7
sigitkusuma <sigitsda71@gmail.com>
2020-12-21 23:16:51 +0700

    adding sign in and register sistem

--- app/src/main/java/com/example/laporankeuangan/LoginActivity.java
+++ app/src/main/java/com/example/laporankeuangan/LoginActivity.java
@@ -1,18 +1,80 @@
 package com.example.laporankeuangan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "Error";
+    private FirebaseAuth mAuth;
+    Button btnLogin;
+    TextView registerText;
+    EditText fillUsername, fillPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        registerText = findViewById(R.id.tv_create_account);
+        btnLogin = findViewById(R.id.btn_login);
+        fillUsername = findViewById(R.id.et_username);
+        fillPassword = findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String username = fillUsername.getText().toString();
+               String password = fillPassword.getText().toString();
+
+               if (username.equals("")){
+                   Toast.makeText(LoginActivity.this, "Silahkan masukan username anda", Toast.LENGTH_SHORT).show();
+               }else if (password.equals("")){
+                   Toast.makeText(LoginActivity.this, "Masukan Password Anda", Toast.LENGTH_SHORT).show();
+               }else {
+                   mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                       @Override
+                       public void onComplete(@NonNull Task<AuthResult> task) {
+                           if (task.isSuccessful()){
+                               FirebaseUser user = mAuth.getCurrentUser();
+                               Toast.makeText(LoginActivity.this, "Authentication Successfull", Toast.LENGTH_SHORT).show();
+                               Intent goPayment = new Intent(LoginActivity.this, PaymentActivity.class);
+                               startActivity(goPayment);
+                               finish();
+                           }else {
+                               Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                           }
+                       }
+                   });
+               }
+            }
+        });
+
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(register);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/laporankeuangan/RegisterActivity.java
+++ app/src/main/java/com/example/laporankeuangan/RegisterActivity.java
@@ -1,14 +1,71 @@
 package com.example.laporankeuangan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText fillUsername, fillPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        fillUsername = findViewById(R.id.et_username);
+        fillPassword = findViewById(R.id.et_password);
+
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = fillUsername.getText().toString();
+                String password = fillPassword.getText().toString();
+
+                if (username.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Username wajib di Isi", Toast.LENGTH_SHORT).show();
+                }else if (password.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Password wajib di Isi", Toast.LENGTH_SHORT).show();
+                }else {
+                    mAuth.createUserWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(RegisterActivity.this, "Authentication Successfuly", Toast.LENGTH_SHORT).show();
+                                        Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(goLogin);
+                                    }else {
+                                        Toast.makeText(RegisterActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent goBack = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(goBack);
+        finish();
     }
 }
